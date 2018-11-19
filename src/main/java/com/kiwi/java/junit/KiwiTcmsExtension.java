a779a75fc8a481118261aa56901f1748c88b50f1
Aneta Petkova <aneta.petkova@sumup.com>
2018-11-19 16:34:27 +0200

    Junit Extension class added

---
+++ src/main/java/com/kiwi/java/junit/KiwiTcmsExtension.java
@@ -0,0 +1,46 @@
+package com.kiwi.java.junit;
+
+import com.kiwi.java.model.TestMethod;
+import org.junit.jupiter.api.TestInfo;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.*;
+import org.junit.jupiter.engine.discovery.predicates.IsTestMethod;
+import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+public class KiwiTcmsExtension extends SummaryGeneratingListener  implements AfterAllCallback, AfterEachCallback, BeforeAllCallback {
+
+    private List<TestMethod> tests;
+
+    public void beforeAll(ExtensionContext context){
+        tests = new ArrayList<>();
+    }
+
+    public void afterAll(ExtensionContext context){
+        System.out.println(TestMethod.toJSONArrayString(tests));
+        //check for existing test plan
+        //if no, create test plan
+        //add results
+    }
+
+    public void afterEach(ExtensionContext context){
+        if (context.getTestMethod().isPresent()) {
+            Method method = context.getTestMethod().get();
+            TestMethod test = new TestMethod();
+            test.name = method.getName();
+            if (context.getExecutionException().isPresent()){
+                test.exception = context.getExecutionException().get();
+                test.result = "FAIL";
+            }
+            else {
+                test.result = "PASS";
+            }
+            test.containingClass = method.getDeclaringClass().getSimpleName();
+            tests.add(test);
+        }
+    }
+
+}
---
+++ src/main/java/com/kiwi/java/model/TestMethod.java
@@ -0,0 +1,32 @@
+package com.kiwi.java.model;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import net.minidev.json.JSONObject;
+
+import java.util.List;
+
+public class TestMethod {
+    public String name;
+    public String containingClass;
+    public String result;
+    public Throwable exception;
+
+    public String toJSONString() {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            return mapper.writeValueAsString(this);
+        } catch (JsonProcessingException e) {
+            return null;
+        }
+    }
+
+    public static String toJSONArrayString(List<TestMethod> list){
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            return mapper.writeValueAsString(list);
+        } catch (JsonProcessingException e) {
+            return null;
+        }
+    }
+}
