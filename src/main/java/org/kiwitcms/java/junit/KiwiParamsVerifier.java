44c9a57111a36c5c8cc15c59acaf8f6048b48233
Mr. Senko <atodorov@mrsenko.com>
2019-01-26 02:05:17 +0200

    Attribute copyright to @apetkova

--- src/main/java/org/kiwitcms/java/junit/KiwiParamsVerifier.java
+++ src/main/java/org/kiwitcms/java/junit/KiwiParamsVerifier.java
@@ -1,3 +1,7 @@
+// Copyright (c) 2018-2019 Aneta Petkova <aneta.v.petkova@gmail.com>
+
+// Licensed under the GPLv3: https://www.gnu.org/licenses/gpl.html
+
 package org.kiwitcms.java.junit;
 
 public class KiwiParamsVerifier {
--- src/main/java/org/kiwitcms/java/model/TestCaseRun.java
+++ src/main/java/org/kiwitcms/java/model/TestCaseRun.java
@@ -1,3 +1,7 @@
+// Copyright (c) 2018-2019 Aneta Petkova <aneta.v.petkova@gmail.com>
+
+// Licensed under the GPLv3: https://www.gnu.org/licenses/gpl.html
+
 package org.kiwitcms.java.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
