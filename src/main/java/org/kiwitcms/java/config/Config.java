f2bb64948a958f938d3f93571bb183b15e82e6ab
Aneta Petkova <aneta.petkova@sumup.com>
2019-01-26 22:34:57 -0200

    Config refactoring

--- src/main/java/org/kiwitcms/java/config/Config.java
+++ src/main/java/org/kiwitcms/java/config/Config.java
@@ -5,24 +5,39 @@ import org.apache.commons.configuration2.builder.fluent.Configurations;
 import org.apache.commons.configuration2.ex.ConfigurationException;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.util.Optional;
+import java.util.Properties;
 
 public class Config {
 
-    private Configuration config;
+    private Properties config;
     private static Config instance;
 
-    private Config(){
-        Configurations configs = new Configurations();
-        try
-        {
-            config = configs.properties(new File("kiwi.tcms.conf"));
-            // access configuration properties
+    private Object getValue(String name, Class<?> type) {
+        String value = config.getProperty(name);
+        if (value == null)
+            return null;
+        if (type == String.class)
+            return value;
+        if (type == boolean.class)
+            return Boolean.parseBoolean(value);
+        if (type == int.class)
+            return Integer.parseInt(value);
+        if (type == float.class)
+            return Float.parseFloat(value);
+        throw new IllegalArgumentException("Unknown configuration value type: " + type.getName());
+    }
 
-        }
-        catch (ConfigurationException cex)
-        {
-            // Something went wrong
+    private Config(){
+        config = new Properties();
+        try {
+            config.load(new FileInputStream(new File("kiwi.tcms.conf")));
+        }catch(java.io.IOException fnfe){
+            fnfe.printStackTrace();
         }
     }
 
@@ -35,37 +50,42 @@ public class Config {
     }
 
     public String getKiwiHost(){
-        return config.getString("TCMS_API_URL");
+        return config.getProperty("TCMS_API_URL");
     }
 
     public String getKiwiUsername(){
-        return config.getString("TCMS_USERNAME");
+        return config.getProperty("TCMS_USERNAME");
     }
 
     public String getKiwiPassword(){
-        return config.getString("TCMS_PASSWORD");
+        return config.getProperty("TCMS_PASSWORD");
     }
 
-    public String getKiwiRunId(){
-        return config.getString("TCMS_RUN_ID");
+    public Integer getKiwiRunId(){
+        String runId = config.getProperty("TCMS_RUN_ID");
+        if (runId.isEmpty()){
+            return null;
+        } else {
+            return Integer.getInteger(runId);
+        }
     }
 
     public String getProduct(){
         return
                 Optional.ofNullable(System.getenv("TRAVIS_REPO_SLUG")).
-                        orElse(config.getString("TCMS_PRODUCT"));
+                        orElse(config.getProperty("TCMS_PRODUCT"));
     }
 
     public String getProductVersion(){
         return
                 Optional.ofNullable(System.getenv("TRAVIS_COMMIT")).
-                        orElse(config.getString("TCMS_PRODUCT_VERSION"));
+                        orElse(config.getProperty("TCMS_PRODUCT_VERSION"));
     }
 
     public String getKiwiBuild(){
         return
             Optional.ofNullable(System.getenv("TRAVIS_BUILD_NUMBER")).
-                    orElse(config.getString("TCMS_BUILD"));
+                    orElse(config.getProperty("TCMS_BUILD"));
 
     }
 
