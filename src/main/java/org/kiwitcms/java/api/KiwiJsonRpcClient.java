8818cb9353b95e66dd2b49420228f8c5fce7d6af
Aneta Petkova <aneta.petkova@sumup.com>
2019-01-27 11:24:34 -0200

    Extended export capabilities

--- src/main/java/org/kiwitcms/java/api/KiwiJsonRpcClient.java
+++ src/main/java/org/kiwitcms/java/api/KiwiJsonRpcClient.java
@@ -1,14 +1,14 @@
 package org.kiwitcms.java.api;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.kiwitcms.java.model.TestCase;
+import org.kiwitcms.java.config.Config;
+import org.kiwitcms.java.model.*;
 import com.thetransactioncompany.jsonrpc2.JSONRPC2Request;
 import com.thetransactioncompany.jsonrpc2.client.JSONRPC2Session;
 import com.thetransactioncompany.jsonrpc2.client.JSONRPC2SessionException;
 import net.minidev.json.JSONArray;
 import net.minidev.json.JSONObject;
-import org.kiwitcms.java.model.TestCaseRun;
-import org.kiwitcms.java.model.TestRun;
 
 import java.io.IOException;
 import java.util.Arrays;
@@ -16,7 +16,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 
-public class KiwiJsonRpcClient extends BaseRpcClient{
+public class KiwiJsonRpcClient extends BaseRpcClient {
 
     private static final String LOGIN_METHOD = "Auth.login";
     private static final String LOGOUT_METHOD = "Auth.logout";
@@ -24,9 +24,20 @@ public class KiwiJsonRpcClient extends BaseRpcClient{
     private static final String CREATE_RUN_METHOD = "TestRun.create";
     private static final String CREATE_TC_METHOD = "TestCase.create";
     private static final String ADD_TC_TO_RUN_METHOD = "TestRun.add_case";
+    private static final String RUN_FILTER = "TestRun.filter";
+    private static final String PRODUCT_FILTER = "Product.filter";
+    private static final String CREATE_PRODUCT_METHOD = "Product.create";
+    private static final String BUILD_FILTER = "Build.filter";
+    private static final String CREATE_BUILD_METHOD = "Build.create";
+    private static final String ADD_TC_TO_PLAN_METHOD = "TestPlan.add_case";
+    private static final String CREATE_PLAN_METHOD = "TestPlan.create";
     private static final String TEST_PLAN_FILTER = "TestPlan.filter";
+    private static final String TEST_CASE_RUN_FILTER = "TestCaseRun.filter";
+    private static final String CREATE_TC_RUN_METHOD = "TestCaseRun.create";
+    private static final String UPDATE_TC_RUN_METHOD = "TestCaseRun.update";
 
-    public TestCase createNewTC(int category, int product, String summary){
+
+    public TestCase createNewTC(int category, int product, String summary) {
         Map<String, Object> params = new HashMap<>();
         //category: Functional - 76
         params.put("category", category);
@@ -35,10 +46,9 @@ public class KiwiJsonRpcClient extends BaseRpcClient{
         params.put("summary", summary);
         //CONFIRMED
         params.put("case_status", 2);
-        params.put("is_automated", 1);
+        params.put("is_automated", "true");
         params.put("priority", 9);
-        JSONObject json = (JSONObject) callPosParamService(CREATE_TC_METHOD, Arrays.asList((Object)params));
-//        return (TestCase) callValueNameParamService(CREATE_TC_METHOD, params);
+        JSONObject json = (JSONObject) callPosParamService(CREATE_TC_METHOD, Arrays.asList((Object) params));
         try {
             return new ObjectMapper().readValue(json.toJSONString(), TestCase.class);
         } catch (IOException e) {
@@ -47,15 +57,19 @@ public class KiwiJsonRpcClient extends BaseRpcClient{
         }
     }
 
-    public TestRun createNewTR(int build, int manager, int plan, String summary){
+    public TestRun createNewRun(int build, String manager, int plan, String summary) {
         Map<String, Object> params = new HashMap<>();
-        //category: Functional - 76
         params.put("build", build);
         params.put("manager", manager);
-        // X-Product - 8
         params.put("plan", plan);
         params.put("summary", summary);
-        return (TestRun) callValueNameParamService(CREATE_TC_METHOD, params);
+        JSONObject json = (JSONObject) callPosParamService(CREATE_RUN_METHOD, Arrays.asList((Object) params));
+        try {
+            return new ObjectMapper().readValue(json.toJSONString(), TestRun.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     public TestCase[] getRunIdTestCases(int runId) {
@@ -64,11 +78,11 @@ public class KiwiJsonRpcClient extends BaseRpcClient{
             return new ObjectMapper().readValue(jsonArray.toJSONString(), TestCase[].class);
         } catch (IOException e) {
             e.printStackTrace();
-            return null;
+            return new TestCase[0];
         }
     }
 
-    public TestCaseRun addTestCaseToRunId(int runId, int caseId){
+    public TestCaseRun addTestCaseToRunId(int runId, int caseId) {
         Map<String, Object> params = new HashMap<>();
         params.put("run_id", runId);
         params.put("case_id", caseId);
@@ -82,16 +96,180 @@ public class KiwiJsonRpcClient extends BaseRpcClient{
         }
     }
 
-    public boolean runExists(int runId){
+    public boolean planExists(int planId) {
         Map<String, Object> filter = new HashMap<>();
-        filter.put("pk", runId);
-        JSONArray jsonArray = (JSONArray)callPosParamService(TEST_PLAN_FILTER, Arrays.asList((Object)filter));
-        return ! jsonArray.isEmpty();
+        filter.put("pk", planId);
+        JSONArray jsonArray = (JSONArray) callPosParamService(TEST_PLAN_FILTER, Arrays.asList((Object) filter));
+        return !jsonArray.isEmpty();
+    }
+
+    public Integer getProductId(String name) {
+        Map<String, Object> filter = new HashMap<>();
+        filter.put("name", name);
+        JSONArray jsonArray = (JSONArray) callPosParamService(PRODUCT_FILTER, Arrays.asList((Object) filter));
+        if (jsonArray.isEmpty()) {
+            return null;
+        } else {
+            try {
+                Product[] product = new ObjectMapper().readValue(jsonArray.toJSONString(), Product[].class);
+                return product[0].getId();
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+    }
+
+    //TODO: investigate error 'Method not found: "Product.create"'
+    public Product createNewProduct(String name) {
+        Map<String, Object> params = new HashMap<>();
+        params.put("name", name);
+
+        JSONObject json = (JSONObject) callPosParamService(CREATE_PRODUCT_METHOD, Arrays.asList((Object) params));
+        try {
+            return new ObjectMapper().readValue(json.toJSONString(), Product.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public TestRun getRun(int runId) {
+        Map<String, Object> filter = new HashMap<>();
+        filter.put("run_id", runId);
+
+        JSONArray jsonArray = (JSONArray) callPosParamService(RUN_FILTER, Arrays.asList((Object) filter));
+        if (jsonArray.isEmpty()) {
+            return null;
+        } else {
+            try {
+                TestRun[] run = new ObjectMapper().readValue(jsonArray.toJSONString(), TestRun[].class);
+                return run[0];
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+    }
+
+    public TestPlan createNewTP(int productId, String name, int type) {
+        Map<String, Object> params = new HashMap<>();
+        params.put("product", productId);
+        params.put("type", type);
+        params.put("default_product_version", 0);
+        params.put("product_version", 1);
+        params.put("text", "WIP");
+        params.put("is_active", true);
+        params.put("name", name);
+        JSONObject json = (JSONObject) callPosParamService(CREATE_PLAN_METHOD, Arrays.asList((Object) params));
+        try {
+            return new ObjectMapper().readValue(json.toJSONString(), TestPlan.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public void addTestCaseToPlan(int planId, int caseId) {
+        Map<String, Object> params = new HashMap<>();
+        params.put("plan_id", planId);
+        params.put("case_id", caseId);
+
+        callNameParamService(ADD_TC_TO_PLAN_METHOD, params);
+    }
+
+    //TODO: Not sure why I needed this
+    public Integer getBuildId() {
+        Map<String, Object> filter = new HashMap<>();
+        filter.put("build_id", 33);
+        filter.put("product_id", 1);
+
+        JSONArray jsonArray = (JSONArray) callPosParamService(BUILD_FILTER, Arrays.asList((Object) filter));
+        if (jsonArray.isEmpty()) {
+            return null;
+        } else {
+            try {
+                Build[] build = new ObjectMapper().readValue(jsonArray.toJSONString(), Build[].class);
+                return build[0].getBuildId();
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+    }
+
+    //TODO: Server returned HTTP response code: 403 for URL: https://demo.kiwitcms.org/json-rpc/
+    public Build createNewBuild(String name, String productName) {
+        Map<String, Object> params = new HashMap<>();
+        params.put("name", name);
+        params.put("product", productName);
+        JSONObject json = (JSONObject) callPosParamService(CREATE_BUILD_METHOD, Arrays.asList((Object) params));
+        try {
+            return new ObjectMapper().readValue(json.toJSONString(), Build.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public TestCaseRun getTestCaseRun(Map<String, Object> filter) {
+        JSONArray jsonArray = (JSONArray) callPosParamService(TEST_CASE_RUN_FILTER, Arrays.asList((Object) filter));
+        if (jsonArray.isEmpty()) {
+            return null;
+        } else {
+            System.out.println(jsonArray.toJSONString());
+            try {
+                TestCaseRun[] tcRun = new ObjectMapper().readValue(jsonArray.toJSONString(), TestCaseRun[].class);
+                return tcRun[0];
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+    }
+
+    public TestCaseRun createTestCaseRun(int runId, int caseId, int build, int status) {
+        Map<String, Object> params = new HashMap<>();
+        params.put("run", runId);
+        params.put("case", caseId);
+        params.put("build", build);
+        params.put("status", status);
+//        TODO: why not working?
+//        params.put("tested_by", user);
+        JSONObject json = (JSONObject) callPosParamService(CREATE_TC_RUN_METHOD, Arrays.asList((Object) params));
+        try {
+            System.out.println(json.toJSONString());
+            return new ObjectMapper().readValue(json.toJSONString(), TestCaseRun.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public TestCaseRun updateTestCaseRun(int tcRunId, int status) {
+        Map<String, Object> values = new HashMap<>();
+        values.put("status", status);
+
+        Map <String, Object> params = new HashMap<>();
+        params.put("case_run_id", tcRunId);
+        params.put("values", values);
+        JSONObject json = (JSONObject) callNameParamService(UPDATE_TC_RUN_METHOD, params);
+        try {
+            return new ObjectMapper().readValue(json.toJSONString(), TestCaseRun.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     public String login(String username, String password) {
-        sessionId = (String) callPosParamService(LOGIN_METHOD, Arrays.asList((Object) username, (Object) password));
+        sessionId = (String) callPosParamService(LOGIN_METHOD, Arrays.asList(username, password));
         return sessionId;
+    }
+
+    //TODO: Seems a bad practice, remove/replace
+    public String login() {
+        return login(Config.getInstance().getKiwiUsername(), Config.getInstance().getKiwiPassword());
     }
 
     public void logout() {
--- src/main/java/org/kiwitcms/java/junit/TestDataEmitter.java
+++ src/main/java/org/kiwitcms/java/junit/TestDataEmitter.java
@@ -3,22 +3,95 @@ package org.kiwitcms.java.junit;
 import org.kiwitcms.java.api.KiwiJsonRpcClient;
 import org.kiwitcms.java.config.Config;
 import org.kiwitcms.java.model.TestCase;
+import org.kiwitcms.java.model.TestCaseRun;
+import org.kiwitcms.java.model.TestMethod;
+import org.kiwitcms.java.model.TestRun;
+
+import java.time.LocalDate;
+import java.util.*;
 
 public class TestDataEmitter {
 
+    private Integer productId;
+    private Integer planId;
+    private Integer runId;
+
     private KiwiJsonRpcClient client;
+    Config config;
 
-    public TestDataEmitter(){
-        Config config = Config.getInstance();
+    public TestDataEmitter() {
+        config = Config.getInstance();
         client = new KiwiJsonRpcClient();
-        client.login(config.getKiwiUsername(), config.getKiwiPassword());
+        client.login();
     }
 
-    public void emitNewTestCase(String summary){
-        client.createNewTC(76, 1, summary);
+    public int getProductId() {
+        if (productId == null) {
+            productId = client.getProductId(config.getProduct());
+            return productId;
+        } else {
+            return productId;
+        }
     }
 
-    public void closeSession(){
+    public void emitNewTestCase(String summary) {
+        client.createNewTC(76, getProductId(), summary);
+    }
+
+    public void closeSession() {
         client.logout();
+    }
+
+    public int getPlanId() {
+        if (planId == null) {
+            Integer confRunId = config.getKiwiRunId();
+            if (confRunId != null) {
+                TestRun run = client.getRun(confRunId);
+                if (run != null) {
+                    planId = run.getPlan();
+                }
+            }
+            planId = client.createNewTP(getProductId(), String.format("Auto Test Plan [%tF]", LocalDate.now()), 1).getId();
+
+        }
+        return planId;
+    }
+
+    public int getTestRunId() {
+        if (runId == null) {
+            Integer testRun = config.getKiwiRunId();
+            if (testRun != null && client.getRun(testRun) != null) {
+                runId = testRun;
+            } else {
+                runId = client.createNewRun(1, Config.getInstance().getKiwiUsername(),
+                        getPlanId(), String.format("Auto Test Run [%tF]", LocalDate.now())).getId();
+            }
+        }
+        return runId;
+    }
+
+    public void addTestResultsToRun(int runId, List<TestMethod> tests) {
+        TestCase[] existingTests = client.getRunIdTestCases(runId);
+        for (TestMethod test : tests) {
+            Integer matchingCaseId = TestCase.nameExists(test.getKiwiSummary(), existingTests);
+            if (matchingCaseId != null) {
+                Map<String, Object> filter = new HashMap<>();
+                filter.put("case_id", matchingCaseId);
+                filter.put("run_id", runId);
+                TestCaseRun testCaseRun = client.getTestCaseRun(filter);
+                if (testCaseRun != null) {
+                    client.updateTestCaseRun(testCaseRun.getTcRunId(), test.getKiwiStatus());
+                } else {
+                    client.createTestCaseRun(runId, matchingCaseId, 1, test.getKiwiStatus());
+                }
+            } else {
+                TestCase addition = client.createNewTC(76, getProductId(), test.getKiwiSummary());
+                client.addTestCaseToPlan(getPlanId(), addition.getCaseId());
+                TestCaseRun tcr = client.addTestCaseToRunId(runId, addition.getCaseId());
+                client.updateTestCaseRun(tcr.getTcRunId(), test.getKiwiStatus());
+            }
+        }
+
+
     }
 }
