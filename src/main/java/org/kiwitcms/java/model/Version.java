778c898a21c26eba68852049e05839eac341b65d
Aneta Petkova <aneta.petkova@sumup.com>
2019-01-27 12:47:17 -0200

    Added Version to model

---
+++ src/main/java/org/kiwitcms/java/model/Version.java
@@ -0,0 +1,48 @@
+package org.kiwitcms.java.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonSetter;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Version {
+    private int id;
+    private String value;
+    private int product_id;
+    private String product;
+
+    public int getId() {
+        return id;
+    }
+
+    @JsonSetter("id")
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    @JsonSetter("value")
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public int getProduct_id() {
+        return product_id;
+    }
+
+    @JsonSetter("product_id")
+    public void setProduct_id(int product_id) {
+        this.product_id = product_id;
+    }
+
+    public String getProduct() {
+        return product;
+    }
+
+    @JsonSetter("product")
+    public void setProduct(String product) {
+        this.product = product;
+    }
+}
