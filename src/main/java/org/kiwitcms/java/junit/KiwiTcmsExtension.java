bd3145658ae3bebe132ca1845042dda9a048cd2b
Mr. Senko <atodorov@mrsenko.com>
2019-03-17 22:57:07 +0200

    Rename getKiwiHost() to getKiwiUrl()
    because this is what it does

--- src/main/java/org/kiwitcms/java/junit/KiwiTcmsExtension.java
+++ src/main/java/org/kiwitcms/java/junit/KiwiTcmsExtension.java
@@ -23,7 +23,7 @@ public class KiwiTcmsExtension extends SummaryGeneratingListener  implements Aft
 
     public void beforeAll(ExtensionContext context) throws ConfigurationException {
         Config config = Config.getInstance();
-        if (config.getKiwiHost().isEmpty() || config.getKiwiUsername().isEmpty() || config.getKiwiPassword().isEmpty()){
+        if (config.getKiwiUrl().isEmpty() || config.getKiwiUsername().isEmpty() || config.getKiwiPassword().isEmpty()){
             throw new ConfigurationException("API URL, username and password must be configured");
         }
         tests = new ArrayList<>();
