ab707348395ceba2759145e7312e3c148aad1561
Mr. Senko <atodorov@mrsenko.com>
2019-04-21 22:11:20 +0300

    Merge KiwiProductJsonRpcClient with RpcClient
    because it really doesn't make sense to have several layers of
    abstraction before we get to the results.

--- src/main/java/org/kiwitcms/java/api/RpcClient.java
+++ src/main/java/org/kiwitcms/java/api/RpcClient.java
@@ -6,11 +6,18 @@ package org.kiwitcms.java.api;
 
 import org.kiwitcms.java.config.Config;
 import org.kiwitcms.java.model.*;
+
 import com.thetransactioncompany.jsonrpc2.JSONRPC2Request;
 import com.thetransactioncompany.jsonrpc2.client.JSONRPC2Session;
 import com.thetransactioncompany.jsonrpc2.client.JSONRPC2SessionException;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import net.minidev.json.JSONObject;
 import net.minidev.json.JSONArray;
 
+import java.io.IOException;
+
+import java.util.HashMap;
 import java.util.Arrays;
 import java.util.Map;
 
@@ -20,13 +27,19 @@ public class RpcClient extends BaseRpcClient {
     public static final String LOGIN_METHOD = "Auth.login";
     public static final String LOGOUT_METHOD = "Auth.logout";
 
+    private static final String PRODUCT_FILTER = "Product.filter";
+    private static final String CREATE_PRODUCT_METHOD = "Product.create";
+    private static final String BUILD_FILTER = "Build.filter";
+    private static final String CREATE_BUILD_METHOD = "Build.create";
+    private static final String CREATE_VERSION_METHOD = "Version.create";
+    private static final String VERSION_FILTER = "Version.filter";
+    private static final String PRIORITY_FILTER = "Priority.filter";
+    private static final String CATEGORY_FILTER = "Category.filter";
 
-    private KiwiProductJsonRpcClient productClient;
     private KiwiTestingJsonRpcClient testingClient;
 
     public RpcClient(){
         super();
-        productClient = new KiwiProductJsonRpcClient();
         testingClient = new KiwiTestingJsonRpcClient();
     }
 
@@ -103,36 +116,117 @@ public class RpcClient extends BaseRpcClient {
        return testingClient.updateTestExecution(tcRunId, status);
     }
 
+
     public Integer getProductId(String name) {
-        return productClient.getProductId(name);
+        Map<String, Object> filter = new HashMap<>();
+        filter.put("name", name);
+        JSONArray jsonArray = (JSONArray) executeViaPositionalParams(PRODUCT_FILTER, Arrays.asList((Object) filter));
+        if (jsonArray.isEmpty()) {
+            return null;
+        } else {
+            try {
+                Product[] product = new ObjectMapper().readValue(jsonArray.toJSONString(), Product[].class);
+                return product[0].getId();
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
     }
 
     public Product createNewProduct(String name) {
-        return productClient.createNewProduct(name);
+        Map<String, Object> params = new HashMap<>();
+
+        // TODO: move classification filtering in emitter
+        // get the first possible classification
+        JSONArray jsonArray = (JSONArray) executeViaPositionalParams("Classification.filter", Arrays.asList((Object) params));
+        Object classificationId = ((JSONObject) jsonArray.get(0)).get("id");
+
+        params.put("name", name);
+        params.put("classification_id", classificationId);
+
+        JSONObject json = (JSONObject) executeViaPositionalParams(CREATE_PRODUCT_METHOD, Arrays.asList((Object) params));
+        try {
+            return new ObjectMapper().readValue(json.toJSONString(), Product.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     public Build[] getBuilds(Map<String, Object> filter) {
-        return productClient.getBuilds(filter);
+        JSONArray jsonArray = (JSONArray) executeViaPositionalParams(BUILD_FILTER, Arrays.asList((Object) filter));
+        if (jsonArray.isEmpty()) {
+            return new Build[0];
+        } else {
+            try {
+                Build[] builds = new ObjectMapper().readValue(jsonArray.toJSONString(), Build[].class);
+                return builds;
+            } catch (IOException e) {
+                e.printStackTrace();
+                return new Build[0];
+            }
+        }
     }
 
     public Build createBuild(String name, int productId) {
-        return productClient.createBuild(name, productId);
+        Map<String, Object> params = new HashMap<>();
+        params.put("name", name);
+        params.put("product", productId);
+        JSONObject json = (JSONObject) executeViaPositionalParams(CREATE_BUILD_METHOD, Arrays.asList((Object) params));
+        try {
+            return new ObjectMapper().readValue(json.toJSONString(), Build.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     public Version[] getVersions(Map<String, Object> filter) {
-       return productClient.getVersions(filter);
+        JSONArray jsonArray = (JSONArray) executeViaPositionalParams(VERSION_FILTER, Arrays.asList((Object) filter));
+        if (jsonArray.isEmpty()) {
+            return new Version[0];
+        } else {
+            try {
+                Version[] versions = new ObjectMapper().readValue(jsonArray.toJSONString(), Version[].class);
+                return versions;
+            } catch (IOException e) {
+                e.printStackTrace();
+                return new Version[0];
+            }
+        }
     }
 
     public Version createProductVersion(String version, int productId) {
-       return productClient.createProductVersion(version, productId);
+        Map<String, Object> params = new HashMap<>();
+        params.put("value", version);
+        params.put("product", productId);
+
+        JSONObject json = (JSONObject) executeViaPositionalParams(CREATE_VERSION_METHOD, Arrays.asList((Object) params));
+        try {
+            return new ObjectMapper().readValue(json.toJSONString(), Version.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     public Priority[] getPriority(Map<String, Object> filter) {
-        return productClient.getPriority(filter);
+        JSONArray jsonArray = (JSONArray) executeViaPositionalParams(PRIORITY_FILTER, Arrays.asList((Object) filter));
+        if (jsonArray.isEmpty()) {
+            return new Priority[0];
+        } else {
+            try {
+                return new ObjectMapper().readValue(jsonArray.toJSONString(), Priority[].class);
+            } catch (IOException e) {
+                e.printStackTrace();
+                return new Priority[0];
+            }
+        }
     }
 
     // TODO: Create Category class
     public JSONArray getCategory(Map<String, Object> filter) {
-        return productClient.getCategory(filter);
+        return (JSONArray) executeViaPositionalParams(CATEGORY_FILTER, Arrays.asList((Object) filter));
     }
 }
