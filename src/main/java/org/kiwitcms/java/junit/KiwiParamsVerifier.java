a6dcb4fa3bb01f68d5bcab48e4bbb321c9df99ea
Mr. Senko <atodorov@mrsenko.com>
2019-04-21 22:40:17 +0300

    Remove empty and unused KiwiParamsVerifier

--- src/main/java/org/kiwitcms/java/junit/KiwiParamsVerifier.java
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2018-2019 Aneta Petkova <aneta.v.petkova@gmail.com>
-
-// Licensed under the GPLv3: https://www.gnu.org/licenses/gpl.html
-
-package org.kiwitcms.java.junit;
-
-public class KiwiParamsVerifier {
-
-}
